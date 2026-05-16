c0591334ba09c74f196f309ac7cf3ca5f94b018e
ibtisam12344 <ibtisamtech88@gmail.com>
2026-05-16 15:48:47 +0500

    deployment url issue fixed

--- middleware.js
+++ middleware.js
@@ -1,15 +1,16 @@
 // middleware.js
 import NextAuth from "next-auth";
-import authConfig from "@/app/_lib/auth.config"; // ✅ No mongoose here
+import authConfig from "@/app/_lib/auth.config";
 import { NextResponse } from "next/server";
 
-// Use only the Edge-safe config for the middleware auth wrapper
 const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
   if (!req.auth?.user) {
     console.log("Middleware: Unauthenticated, redirecting...");
-    return NextResponse.redirect(new URL("/login", req.url));
+
+    const loginUrl = new URL("/login", req.nextUrl.origin);
+    return NextResponse.redirect(loginUrl);
   }
   return NextResponse.next();
 });
